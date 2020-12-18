3eb3c696fa675c5ab5e25f627c87d641f92f3b57
jos_aa <jos_aaron@hotmail.com>
2020-12-17 20:35:59 -0600

    agregado el slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        speed: 3000S
+    });
+});
